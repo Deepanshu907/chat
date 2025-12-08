19283475441cab4e658ccfe71561e17f7c0817a3
deepanshukumarjindal <deepanshujindal190@gmail.com>
2025-12-08 21:52:16 +0530

    update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -11,7 +11,7 @@
 
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://chat-backend-h4jy.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
